ed887a9941fc50cc89ebcfea3073e10e36b75172
akdiallo <akdiallo@gainde2000.sn>
2025-07-15 22:58:31 +0000

    Gestion des variables d'environoement

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://rimkalechanchelier:mQRS6W29VZq3mNa7@cluster0.nrb8dmc.mongodb.net/notes-app');
+        const conn = await mongoose.connect(`mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.nrb8dmc.mongodb.net/${process.env.DATABASE_NAME}`);
         console.log('✅ MongoDB connecté:', conn.connection.host);
     } catch (error) {
         console.error('❌ Erreur de connexion MongoDB:', error.message);
